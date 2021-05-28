9c30b5c7b4ae15e64fb4ef01d8508e8b7ce87e03
Abira16 <abinss.17@cse.mrt.ac.lk>
2021-05-28 19:37:39 +0530

    update login page interface

--- abi/login.php
+++ abi/login.php
@@ -1,3 +1,58 @@
+<?php
+session_start();
+
+if(isset($_POST["login"])){
+
+if(!empty($_POST['your_email']) && !empty($_POST['psw'])) {
+    
+	$useremail=$_POST['your_email'];
+	$pass=$_POST['psw'];
+    $db = mysqli_connect("localhost:3307","root","","car_showroom");
+    
+	$query=mysqli_query($db,"SELECT * FROM customer WHERE email= '".$useremail."' AND pass='".$pass."'");
+    
+   	$numrows=mysqli_num_rows($query);
+    
+	if($numrows !=0)
+	{
+        while($row=mysqli_fetch_assoc($query))
+        {
+            $dbuseremail=$row['email'];
+            $dbpass=$row['pass'];
+            $dbusername=$row['name'];    
+	    }
+
+        
+        if($useremail == $dbuseremail && $pass == $dbpass)
+        {
+
+            $_SESSION['s_name']= $dbusername;
+
+            /* Redirect browser */
+            header("Location: indexlogin.php");
+        }
+	} 
+    else 
+    {
+	   $message = "Invalid credentials !";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+	}
+
+} 
+    else {
+	echo "All fields are required!";
+        }
+}
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +66,7 @@
 	<!-- Jquery -->
 	<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
 	<!-- Main Style Css -->
-    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/style1.css"/>
 </head>
 <body class="form-v2">
 	<div class="header">	
@@ -50,43 +105,31 @@
 	<div class="page-content">
 		<div class="form-v2-content">
 			<div class="form-left">
-				<img src="images/reg.jpg" alt="form">
+				<img src="images/login.jpg" alt="form">
 			</div>
 			<form class="form-detail" action="#" method="post" id="myform">
-				<h2>Registration Form</h2>
-				<div class="form-row">
-					<label for="full-name">Full Name:</label>
-					<input type="text" name="full_name" id="full_name" class="input-text" placeholder="ex: Lindsey Wilson">
-				</div>
+				<h2>SIGN IN</h2>
+				
 				<div class="form-row">
 					<label for="your_email">Your Email:</label>
 					<input type="text" name="your_email" id="your_email" class="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
 				</div>
-				<div class="form-row">
-					<label for="address">Address:</label>
-					<input type="text" name="address" id="address" class="input-text" required>
-				</div>
-				<div class="form-row">
-					<label for="phone">Phone Number:</label>
-					<input type="tel" id="phone" name="phone" class="input-text" pattern="[0-9]{10}" required>
-				</div>
+								
 				<div class="form-row">
 					<label for="password">Password:</label>
 					<input type="password" id="psw" name="psw" class="input-text" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
 
-				</div>
-				<div class="form-row">
-					<label for="comfirm-password">Confirm Password:</label>
-					<input type="password" name="confirm_password" class="input-text" id="confirm_password" class="input-text" required>
-				</div>
+				
 				<div class="form-row-last">
-					<input href="login.php" type="submit" name="register" class="register" value="Register">
+					<input href="home.php" type="submit" name="login" class="register" value="Sign In">
 				</div>
 			</form>
 		</div>
 	</div>
+    </div>
 	
-<div class="footer">
+	
+    <div class="footer">
 	<div class="wrap">
 	   <div class="footer-top">				
 				<div class="col_1_of_5 span_1_of_5">
@@ -121,83 +164,9 @@
 	</div>
 </div>		
 
-	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
-	<script>
-		// just for the demos, avoids form submit
-		jQuery.validator.setDefaults({
-		  	debug: true,
-		  	success:  function(label){
-        		label.attr('id', 'valid');
-   		 	},
-		});
-		$( "#myform" ).validate({
-		  	rules: {
-		    	password: "required",
-		    	confirm_password: {
-		      		equalTo: "#password"
-		    	}
-		  	},
-		  	messages: {
-		  		full_name: {
-		  			required: "Please provide an username"
-		  		},
-		  		your_email: {
-		  			required: "Please provide an email"
-		  		},
-				address: {
-					required: "Please provide an address"	
-				},
-				phone: {
-					required: "Please provide a phone number"
-				},
-		  		password: {
-	  				required: "Please provide a password"
-		  		},
-		  		confirm_password: {
-		  			required: "Please provide a password",
-		      		equalTo: "Wrong Password"
-		    	}
-		  	}
-		});
-	</script>
+
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
-
-
-<?php 
-$db=mysqli_connect("localhost:3307","root","","car_showroom");
-
-// REGISTER USER
-if(isset($_POST['reg'])) 
-{
-    
-    
-	// receive all input values from the form
-    $username = $_POST['full_name'];
-	$useremail = $_POST['your_email'];
-	$password = $_POST['psw'];
-    $userphone = $_POST['phone'];
-    $useraddress = $_POST['address'];
-    
-    if($useremail!='' || $username!=''|| $password!=''|| $userphone!=''|| $useraddress!='' )
-    {
-       $query = "CALL register('$username','$useremail','$password','$userphone','$useraddress')";
-        
-		mysqli_query($db, $query);
-        
-        $message = "registration done ! ";
-         echo "<script type='text/javascript'>alert('$message');</script>";
-        
-      
-    }
-	
-		
-
-		
-	
-	
-}
-
- ?>
+    	
+    	
+            
